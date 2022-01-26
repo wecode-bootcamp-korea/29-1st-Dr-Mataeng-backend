2bea669567177c969dab3bee0a68833434449539
jxngbxxm <jxngbxxm@gmail.com>
2022-01-26 15:32:41 +0900

    [29기 강종범][Modify] project modeling(users, products, orders)-users/views.py 오기입 수정

--- users/views.py
+++ users/views.py
@@ -1,14 +1,3 @@
-from django.db import models
+from django.shortcuts import render
 
-class User(models.Model):
-    name         = models.CharField(max_length=50)
-    username     = models.CharField(max_length=100, unique=True)
-    password     = models.CharField(max_length=200)
-    birthday     = models.DateField()
-    email        = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length=30)
-    created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True)
-  
-    class Meta:
-        db_table = "users"
+# Create your views here.
