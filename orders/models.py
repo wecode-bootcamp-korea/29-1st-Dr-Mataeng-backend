b53cb6c6be437fcd846dc2285465efa4b63c7767
jxngbxxm <jxngbxxm@gmail.com>
2022-01-26 14:58:15 +0900

    [29기 강종범][Modify] project modeling(users, products, orders)-recommender 추가

---
+++ orders/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+from users.models    import User
+from products.models import Product_Option
+
+class Cart(models.Model):
+    user           = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_option = models.ForeignKey(Product_Option, on_delete=models.CASCADE)
+    quantity       = models.IntegerField()
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "carts"
+
+class Order_Status(models.Model):
+    status = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "orders_stauses"
+
+class Order_Item(models.Model):
+    product_option = models.ForeignKey(Product_Option, on_delete=models.CASCADE)
+    quantity       = models.IntegerField()
+    order_status   = models.ForeignKey(Order_Status, on_delete=models.CASCADE)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "orders_items"
+
+class Order(models.Model):
+    user         = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=100)
+    order_item   = models.ForeignKey(Order_Item, on_delete=models.CASCADE)
+    order_status = models.ForeignKey(Order_Status, on_delete=models.CASCADE)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "orders"
---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+    name         = models.CharField(max_length=50)
+    username     = models.CharField(max_length=100, unique=True)
+    password     = models.CharField(max_length=200)
+    birthday     = models.DateField()
+    email        = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=30)
+    recommender  = models.CharField(max_length=100, blank=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+  
+    class Meta:
+        db_table = "users"
