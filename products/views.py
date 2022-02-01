502098003d6552d646b97f7119348e5c5022b41a
jxngbxxm <jxngbxxm@gmail.com>
2022-02-01 17:02:41 +0900

    [29기 강종범][Modify] Products-ProductListView(get), urls.py, signup(refactoring)

--- products/views.py
+++ products/views.py
@@ -2,29 +2,23 @@ import json
 
 from django.http  import JsonResponse
 from django.views import View
-from django.db.models import Q
+from django.db.models import Q,F,Sum
 
 from products.models import ProductColor, ProductImage, ProductOption
 
 class ProductListView(View):
     def get(self, request, *args, **kwargs):
         try:
-            gender          = request.GET.get("gender", None)
-            category        = request.GET.get("category", None)
-            color           = request.GET.get("color", None)
-            size            = request.GET.get("size", None)
-            min_price       = request.GET.get("min_price", 10000)
-            max_price       = request.GET.get("max_price", 500000)
-            products_colors = ProductColor.objects.all()
-            products_images = ProductImage.objects.all()
-            sort            = request.GET.get("sort", "인기순")
-
-            sort_dict = {
-                "인기순"      : "id",
-                "신상품 순"    : "-id",
-                # "높은 가격 순" : "-price",
-                # "낮은 가격 순" : "price",
-                }
+            gender           = request.GET.get("gender", None)
+            category         = request.GET.get("category", None)
+            color            = request.GET.get("color", None)
+            size             = request.GET.get("size", None)
+            min_price        = request.GET.get("min_price", 10000)
+            max_price        = request.GET.get("max_price", 500000)
+            products_colors  = ProductColor.objects.all()
+            products_images  = ProductImage.objects.all()
+            products_options = ProductOption.objects.all()
+            sort             = request.GET.get("sort", None)
 
             q  = Q()
             q &= Q(productoption__product_color__product__price__gte=min_price)
@@ -42,7 +36,7 @@ class ProductListView(View):
                 size = size.split(',')
                 q &= Q(productoption__size__name__in=size)
 
-            products_colors = products_colors.filter(q).distinct().order_by(sort_dict[sort])
+            products_colors = products_colors.filter(q).distinct()
     
             data = [{
                 "id"            : product_color.id,
@@ -51,7 +45,21 @@ class ProductListView(View):
                                   .first().image_url,
                 "price"         : int(product_color.product.price),
                 "product_color" : product_color.color.name,
+                "total_stock"   : products_options.filter(product_color_id=product_color.id)\
+                                  .aggregate(Sum('stock'))['stock__sum'],
             }for product_color in products_colors]
+
+            if sort == "인기순":
+                data = sorted(data, key= lambda dict : dict['total_stock'])
+
+            if sort == "신상품 순":
+                data = sorted(data, key= lambda dict : dict['id'], reverse=True)
+
+            if sort == "높은 가격 순":
+                data = sorted(data, key= lambda dict : dict['price'], reverse=True)
+
+            if sort == "낮은 가격 순":
+                data = sorted(data, key= lambda dict : dict['price'])
 
             return JsonResponse({"result" : data}, status=200)
         except KeyError:
