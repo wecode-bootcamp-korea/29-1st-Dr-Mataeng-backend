760aa9f9c0653156f420aa879af022175c5450f3
jxngbxxm <jxngbxxm@gmail.com>
2022-02-07 12:41:25 +0900

    [29기 강종범][Add] CartView(get), urls.py

--- carts/views.py
+++ carts/views.py
@@ -9,36 +9,21 @@ from products.models  import ProductOption
 
 class CartView(View):
     @LoginConfirm.login_decorator
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         try:
-            data       = json.loads(request.body)
-            user       = request.user
-            product_id = data['product_id']
-            size       = data['size']
-            quantity   = int(data['quantity'])
+            user  = request.user
+            carts = Cart.objects.filter(user=user)
 
-            if not ProductOption.objects.filter(
-                product_color_id = product_id, 
-                size__name       = size
-                ).exists():
-                return JsonResponse({"message" : "PRODUCT OR SIZE DOSE NOT EXIST"}, status=400)
-
-            product_option = ProductOption.objects.get(
-                product_color_id = product_id,
-                size__name       = size
-                )
-
-            if quantity > int(product_option.stock) or quantity < 1:
-                return JsonResponse({"message" : "INVALID QUANTITY"}, status=400)
-
-            cart, is_created_flag = Cart.objects.get_or_create(
-                user_id           = user.id,
-                product_option    = product_option,
-                quantity          = quantity
-            )
-            cart.save()
-            return JsonResponse({"message" : "SUCCESS"}, status=201) 
-        except KeyError:
-            return JsonResponse({"message" : "KEY ERROR"}, status=400)
+            data = [{
+                'cart_id'       : cart.id,
+                'product_id'    : cart.product_option.product_color.id,
+                'product_name'  : cart.product_option.product_color.product.name,
+                'product_image' : cart.product_option.product_color.products_images.first().image_url,
+                'product_color' : cart.product_option.product_color.color.name,
+                'product_size'  : cart.product_option.size.name,
+                'quantity'      : cart.quantity,
+                'price'         : int(cart.product_option.product_color.product.price * cart.quantity)
+            }for cart in carts]
+            return JsonResponse({"result" : data}, status=200)
         except Exception as e:
             return JsonResponse({"message" : getattr(e,"message",str(e))}, status=400)
